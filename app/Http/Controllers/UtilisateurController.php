57f96ca33b24f9fdcbc94c3e9baf473364e91e4c
nargil07 <nargilcorp@gmail.com>
2017-03-13 14:41:28 +0100

    changement

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -10,20 +10,23 @@ class UtilisateurController extends Controller
 {
     public function register(Request $request)
     {
-        $payload = $request->attributes->get('payload');
-        if(!is_null($payload)){
-            $metierUtilisateur = new MetierUtilisateur();
-            $personne = $metierUtilisateur->getUtilisateurByEmail($payload['email']);
-            if(is_null($personne)){
-                $personne = new Utilisateur();
-                $personne->nom = $payload['family_name'];
-                $personne->prenom = $payload['given_name'];
-                $personne->email = $payload['email'];
-                $personne->picture = $payload['picture'];
-                return response()->json($personne->save());
+        if($request->attributes->has('payload')){
+            $payload = $request->attributes->get('payload');
+            if(!is_null($payload)){
+                $metierUtilisateur = new MetierUtilisateur();
+                $personne = $metierUtilisateur->getUtilisateurByEmail($payload['email']);
+                if(is_null($personne)){
+                    $personne = new Utilisateur();
+                    $personne->nom = $payload['family_name'];
+                    $personne->prenom = $payload['given_name'];
+                    $personne->email = $payload['email'];
+                    $personne->picture = $payload['picture'];
+                    return response()->json($personne->save());
+                }
+                return response()->json(true);
             }
-            return response()->json(true);
         }
+
         return response()->json(false);
     }
 }
