6d0d52eca4a4f41978db5cc9b5fea7fcbd25a926
nargil07 <nargilcorp@gmail.com>
2017-04-25 11:49:54 +0200

    Changement des routes

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 Route::post ('/utilisateur',                            ["uses" => "UtilisateurController@register"]                );
-Route::post ('/{idUtilisateur}/messages',               ["uses" => "MessageController@newMessage"]                 );
-Route::get  ('/{idUtilisateur}/messages',               ["uses" => "MessageController@getMessagesFromUser"]        );
-Route::put  ('/{idUtilisateur}/messages/{idMessage}',   ["uses" => "MessageController@ramasserMessage"]            );
-Route::get  ('/{idUtilisateur}/messages/proche',        ["uses" => "MessageController@getMessagesFromLocalisation"]);
+Route::post ('/{idUtilisateur}/messages',               ["uses" => "MessageController@newMessage"]                  );
+Route::get  ('/{idUtilisateur}/messages',               ["uses" => "MessageController@getMessagesFromUser"]         );
+Route::put  ('/{idUtilisateur}/messages/{idMessage}',   ["uses" => "MessageController@ramasserMessage"]             );
+Route::get  ('/{idUtilisateur}/messages/proche',        ["uses" => "MessageController@getMessagesFromLocalisation"] );
